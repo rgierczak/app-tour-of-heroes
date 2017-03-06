a64a52a2be0d8b0fbe6fe3f07f068790039a3fed
Rafał Gierczak <grime85@gmail.com>
2017-03-06 23:01:14 +0100

    Show single hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 import '../assets/css/styles.css';
 
+export class Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+
+export class AppComponent {
+    title: 'Tour of Heroes';
+    hero: Hero = {
+        id: 1,
+        name: 'windstorm'
+    }
+}
