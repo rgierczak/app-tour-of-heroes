4053c22751bb1c4b5dda251bb43da7a0c19ec94d
Rafał Gierczak <grime85@gmail.com>
2017-03-06 23:37:26 +0100

    Added master/details structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,19 @@ export class Hero {
     name: string;
 }
 
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
@@ -15,8 +28,10 @@ export class Hero {
 
 export class AppComponent {
     title = 'Tour of Heroes';
-    hero: Hero = {
-        id: 1,
-        name: 'windstorm'
+    heroes = HEROES;
+    selectedHero: Hero;
+    
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
     }
 }
