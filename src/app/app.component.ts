bd252f57aa47c775d5f370520146ed9550a75398
Rafał Gierczak <grime85@gmail.com>
2017-03-20 18:00:16 +0100

    Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
-import '../assets/css/styles.css';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    providers: [HeroService]
+    styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent {
     title = 'Tour of Heroes';
-    heroes: Hero[];
-    selectedHero: Hero;
-
-    constructor(private heroService: HeroService) {}
-
-    getHeroes(): void {
-        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-    }
-
-    ngOnInit(): void {
-        this.getHeroes();
-    }
-
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@angular/core';
-
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class HeroService {
     getHeroes(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
+    }
+
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
     }
 }
