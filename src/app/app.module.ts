7f391c35c024a066a838bd5a5731bcbcf422dfdd
Rafał Gierczak <grime85@gmail.com>
2017-04-02 16:10:13 +0200

    Moved components to separated directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,10 @@ import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroSearchComponent } from './hero-search.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroSearchComponent } from './components/hero-search/hero-search.component';
 import { HeroService } from './hero.service';
 
 @NgModule({
--- src/app/components/hero-search/hero-search.component.ts
+++ src/app/components/hero-search/hero-search.component.ts
@@ -11,8 +11,8 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 
-import { HeroSearchService } from './hero-search.service';
-import { Hero } from './hero';
+import { HeroSearchService } from '../../hero-search.service';
+import { Hero } from '../../hero';
 
 @Component({
     selector: 'hero-search',
